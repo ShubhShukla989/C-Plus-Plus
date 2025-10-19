35646b78ee4b87bfb31266749f851c7f1bd05ef7
ShubhShukla989 <priyeshs423@gmail.com>
2025-10-20 00:16:24 +0530

    updated

--- basic.cpp
+++ basic.cpp
@@ -27,3 +27,5 @@ int main(){
     cin>>n;
     if (n%2==0){cout<<"n is even";}
     else {cout<<"n is odd";}}
+
+    
--- conditional.cpp
+++ conditional.cpp
@@ -26,11 +26,11 @@
 // else{cout<<"Area is equal to perimeter";
 // }
 // }
-#include<iostream>
-using namespace std;
-int main(){
-    int x=3,y=5;
-    if (x==3)
-    cout<<"x is 3"<<endl;
-    else (cout<<"y"<<endl);
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=3,y=5;
+//     if (x==3)
+//     cout<<"x is 3"<<endl;
+//     else (cout<<"y"<<endl);
+// }
